dc662c9ccff6fa2987d4d5fd2b09262af072a81d
joariel93 <joariel93@hotmail.com>
2020-04-21 20:41:32 -0300

    Se agregan nubes que se mueven, hay que ver como consigo reconocer la colision

---
+++ saharaEnGlobo/Assets/Scripts/MovimientoNubes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovimientoNubes : MonoBehaviour
+{
+    [Header("Settings")]
+    [SerializeField] private float boundaryLeft = default;
+    [SerializeField] private float boundaryRight = default;
+    [SerializeField] private float speed = default;
+
+    [Header("References")]
+    [SerializeField] private Transform nube = default;
+
+    private void Update()
+    {
+        nube.Translate(new Vector3(Time.deltaTime * speed, 0));
+
+        if (nube.position.x >= boundaryRight)
+        {
+            nube.position = new Vector3(boundaryLeft, nube.position.y);
+        }
+    }
+}
