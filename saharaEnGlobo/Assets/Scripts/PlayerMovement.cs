bc0464616502f6817cae0aec9bdfd13050534393
joariel93 <joariel93@hotmail.com>
2020-04-21 17:41:06 -0300

    El player ya camina

--- saharaEnGlobo/Assets/Scripts/PlayerMovement.cs
+++ saharaEnGlobo/Assets/Scripts/PlayerMovement.cs
@@ -8,18 +8,15 @@ public class PlayerMovement : MonoBehaviour
     public float runSpeed;
     private float moveInput;
 
-
     private Rigidbody2D myBody;
+    private Animator animacion;
 
-
-
-
-
+    private bool facingLeft;
 
     void Awake()
     {
         myBody = GetComponent<Rigidbody2D>();
-
+        animacion = GetComponent<Animator>();
         
     }
 
@@ -28,11 +25,31 @@ public class PlayerMovement : MonoBehaviour
     {
         Movement();
     }
-
+    /// <summary>
+    /// Va a mover al personaje cuando se presionen las flechas, usa la velocidad que le asigne como runSpeed
+    /// </summary>
     void Movement ()
     {
         moveInput = Input.GetAxisRaw("Horizontal")*runSpeed;
         myBody.velocity = new Vector2(moveInput, myBody.velocity.y);
+
+        if (moveInput > 0)
+            animacion.SetFloat("Speed", moveInput);
+        else
+            animacion.SetFloat("Speed", moveInput * -1);
+
+        if (moveInput > 0 && !facingLeft || moveInput < 0 && facingLeft)
+            Flip();
+    }
+    /// <summary>
+    /// Va a voltear al personaje segun para donde este mirando
+    /// </summary>
+    void Flip()
+    {
+        facingLeft = !facingLeft;
+        Vector3 transformScale = transform.localScale;
+        transformScale.x *= -1;
+        transform.localScale = transformScale;
     }
 
 }
