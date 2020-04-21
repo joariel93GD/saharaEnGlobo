1661f9b9557f7735f82dff63fc279b65b3624f0f
joariel93 <joariel93@hotmail.com>
2020-04-20 21:14:04 -0300

    Subo el proyecto en Unity

---
+++ saharaEnGlobo/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float runSpeed;
+    private float moveInput;
+
+
+    private Rigidbody2D myBody;
+
+
+
+
+
+
+    void Awake()
+    {
+        myBody = GetComponent<Rigidbody2D>();
+
+        
+    }
+
+    
+    void FixedUpdate()
+    {
+        Movement();
+    }
+
+    void Movement ()
+    {
+        moveInput = Input.GetAxisRaw("Horizontal")*runSpeed;
+        myBody.velocity = new Vector2(moveInput, myBody.velocity.y);
+    }
+
+}
